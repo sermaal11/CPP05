b455c881c537b261aa86e228e3de9af32acdf1df
Jariskan <sergio.marin.alvarez@gmail.com>
2025-10-16 19:22:12 +0200

    Agregar archivos y definir clases Bureaucrat y Form con gestión de excepciones y validación de grado

---
+++ ex01/include/Form.hpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sergio <sergio@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 18:19:07 by sergio            #+#    #+#             */
+/*   Updated: 2025/10/16 19:16:19 by sergio           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# define RED "\033[31m"
+# define CYAN "\033[36m"
+# define MAGENTA "\033[35m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define RESET "\033[0m"
+
+# include <iostream>
+# include <string>
+# include <exception>
+
+class Bureaucrat;	// Forward declaration para evitar dependencias circulares
+
+class Form
+{
+	private:
+		const std::string	_name;
+		bool				_isSigned;
+		const int			_gradeToSign;
+		const int			_gradeToExecute;
+
+	private:
+		static void _assertLevelInRange(int grade);
+
+	public:
+		//OCF
+		Form(const std::string& name, int gradeToSign, int gradeToExecute);
+		Form(const Form& other);
+		Form& operator=(const Form& other);
+		~Form();
+
+		// Getters
+		const std::string& getName() const;
+		bool getIsSigned() const;
+		int getGradeToSign() const;
+		int getGradeToExecute() const;
+
+		// Signed Form Function
+		void beSigned(const Bureaucrat& bureaucrat);
+
+		// Nested Execptions
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char* what() const throw();	
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char* what() const throw();	
+		};
+		
+};
+
+#endif // FORM_HPP
---
+++ ex01/src/Form.cpp
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sergio <sergio@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 18:19:12 by sergio            #+#    #+#             */
+/*   Updated: 2025/10/16 19:18:43 by sergio           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/Form.hpp"
+#include "../include/Bureaucrat.hpp"
+
+
+// Constructor
+Form::Form(const std::string& name, int gradeToSign, int gradeToExecute)
+	:	_name(name),
+		_isSigned(false),
+		_gradeToSign(gradeToSign),
+		_gradeToExecute(gradeToExecute)
+{
+	_assertLevelInRange(_gradeToSign);
+	_assertLevelInRange(_gradeToExecute);
+	std::cout << GREEN << "Constructor called for " << _name << RESET << std::endl;
+}
+
+// Constructor x copia
+Form::Form(const Form& other)
+	:	_name(other._name),
+		_isSigned(other._isSigned),
+		_gradeToSign(other._gradeToSign),
+		_gradeToExecute(other._gradeToExecute)
+{
+	std::cout << GREEN << "Copy constructor called for " << _name << RESET << std::endl;
+}
+
+// Operador de asigancion
+Form& Form::operator=(const Form& other)
+{
+	if (this != &other)
+	{
+		this->_isSigned = other._isSigned;
+	}
+	return *this;
+}
+
+// Destructor x defecto
+Form::~Form()
+{
+	std::cout << RED << "Destructor called for " << _name << RESET << std::endl;
+}
+
+// Validacion de nivel
+void Form::_assertLevelInRange(int grade)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+}
+
+// Getters
+
+const std::string& Form::getName() const
+{
+	return _name;
+}
+
+bool Form::getIsSigned() const
+{
+	return _isSigned;
+}
+
+int Form::getGradeToSign() const
+{
+	return _gradeToSign;
+}
+
+int Form::getGradeToExecute() const
+{
+	return _gradeToExecute;
+}
+
+// Signed Form Function
+void Form::beSigned(const Bureaucrat& bureaucrat)
+{
+	if (bureaucrat.getGrade() <= _gradeToSign)
+	{
+		_isSigned = true;
+	}
+	else
+		throw GradeTooLowException();
+}
+
+const char* Form::GradeTooHighException::what() const throw()
+{
+	return "Form: grade too high (must be >= 1)";
+}
+
+const char* Form::GradeTooLowException::what() const throw()
+{
+	return "Form: grade too low (must be <= 150 or insufficient to sign)";
+}
